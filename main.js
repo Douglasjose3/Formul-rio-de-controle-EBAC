51f3fc5e42114f0d36691948a2b1c5bddbcdd13b
Douglasjose <douglasjose3@gmail.com>
2023-03-08 00:19:21 -0300

    Formulario de controle

---
+++ main.js
@@ -0,0 +1,41 @@
+const form = document.getElementById('form-AB');
+const campoApreenchido = document.getElementById('campo-A-preenchido');
+let formEValido = false;
+
+function validaA(campoA) {
+    const numeroAComoArray = campoA.split('');
+    return numeroAComoArray.length <= 2;
+}
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    const campoBpreenchido = document.getElementById('campo-B-preenchido');
+    const mensagemSucesso = `O valor: <b>${campoApreenchido.value}</b> está correto camparando com o valor: <b>${campoBpreenchido.value}</b>.`;
+
+    formEValido = validaA(campoApreenchido.value)
+    if (formEValido) {
+        const containermensagemSucesso = document.querySelector('.success-message');
+        containermensagemSucesso.innerHTML = mensagemSucesso;
+        containermensagemSucesso.style.display = 'block';
+
+        campoApreenchido.value = '';
+        campoBpreenchido.value = '';
+    } else {
+        campoApreenchido.style.border = '1px solid red';
+        document.querySelector('.error-message').style.display = 'block';
+    }
+})
+
+campoApreenchido.addEventListener('keyup', function(e) {
+    console.log(e.target.value);
+    formEValido = validaA(e.target.value)
+
+    if (!formEValido) {
+        campoApreenchido.classList.add('error');
+        document.querySelector('.error-message').style.display = 'none';
+    } else {
+        campoApreenchido.classList.remove('error');
+        document.querySelector('.error-message').style.display = 'none';
+    }
+});
